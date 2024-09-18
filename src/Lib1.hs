13f822a3c71311a0c4b253f205adc39c48fab6da
Kvizas <adosileikis@gmail.com>
2024-09-18 10:04:05 +0000

    Auto complete

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,4 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = ["Hello"]
+completions = ["Start Production", "End Production", "Hexacopter", "Quadcopter", "VTOL Drone", "Fixed-Wing Drone", "Check Inventory", "Motor", "Battery", "Frame", "Propeller", "Controller", "Perform Quality Check", "Battery Test", "Flight Test", "Frame Inspection"]
